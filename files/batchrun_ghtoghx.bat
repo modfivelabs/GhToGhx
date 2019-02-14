5a2d1c18be45c9c12ef43907952e4216019a61f3
Rolf Lampa <rolf.lampa@rilnet.com>
2019-02-14 06:08:32 +0100

    Updated command option info in batch file

--- files/batchrun_ghtoghx.bat
+++ files/batchrun_ghtoghx.bat
@@ -2,15 +2,21 @@ REM ----------------------------------------------------------------------------
 REM 	Prefix "+" means verbose (console display)
 REM 	Prefix "-" means !verbose (no or very little console display)
 REM 	Postfix "L" means = Do log to file
+REM 	g means List existing grasshopper files (.gh)
 REM 	X means Convert to .ghx (xml) format
 REM 	x means List ghx files
 REM 	Z means Compress to .ghx.gz  (GZip) format
 REM 	z means List compressed files
-REM 	D means List temp directories for ghx and gzip files
+REM 	d means List temp directories for ghx and gzip files
+REM     .............................................................................
+REM	? means pause (prompt ant wait for a key to be pressed
+REM 	m Toggle menu visibility (relevant only to the Console Windows mode).
 REM ---------------------------------------------------------------------------------
-REM
+
+REM - LOG regular Grasshopper files (.gh)
+.\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +gL
 REM - CREATING Xml versions (ghx)
-.\ghtoghx.exe "D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\" +XL?
+.\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +XL
 REM - CREATING compressed GZip versions (gzip)
 .\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +ZL
 REM - LOG ghx file-lists to disk
@@ -18,10 +24,11 @@ REM - LOG ghx file-lists to disk
 REM - LOG GZip file-lists to disk
 .\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +zL
 REM - LOG Ghx and GZip foldername lists to disk
-.\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +DL
+.\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +dL
 REM - REMOVING ghx folders and files ("L" doesn't play any role here)
 .\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +R
 REM - REMOVING GZip folders and files
 .\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +r
-REM - CLEAR Log files
+REM - CLEAR Log files 
+REM - Also pause (? = prompt) to avid terminating so that a screenshot can be taken
 .\ghtoghx.exe D:\DEV\CAD\GH\GH_Workbench\__ScriptComponents\ +C?
